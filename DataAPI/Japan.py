d40f3473d123df50de71bce224115db026b22912
STONE <sjhstone@live.com>
2022-04-26 01:34:06 +0800

    temporary fix: corrupted Tokyo data

--- DataAPI/Japan.py
+++ DataAPI/Japan.py
@@ -28,6 +28,17 @@ class Japan(OnlineDataSource):
 class Tokyo(Japan):
     def __init__(self):
         super().__init__('tokyo', '東京都', datetime.date(2021, 12, 24))
+    
+    def df_postprocess(self, df):
+        df = super().df_postprocess(df)
+        # fix corrupted data
+        #   2022-04-10  1326924
+        #   2022-04-11 >1326924< -- should be 1331486
+        #   2022-04-12  1338408
+        #   2022-04-13  1346661
+        # see https://stopcovid19.metro.tokyo.lg.jp/zh-cn/cards/number-of-confirmed-cases/
+        df.loc[df['date'] == datetime.date(2022, 4, 11),'acc_cases'] = 1331486
+        return df
 
 class Osaka(Japan):
     def __init__(self):
