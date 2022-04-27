5c8a1c011012d82d924064321cef08f9a5319747
STONE <sjhstone@live.com>
2022-04-27 10:05:23 +0800

    easier to comment out source

--- collect_data.py
+++ collect_data.py
@@ -1,7 +1,13 @@
 from DataAPI import Korea, Japan, China
 # ordered by the date of the first local Omicron case
 
-cities = [Korea.Seoul(), Japan.Osaka(), Japan.Tokyo(), China.HongKong(), China.Shanghai()]
+cities = list()
+cities.append(Korea.Seoul())
+cities.append(Japan.Osaka())
+cities.append(Japan.Tokyo())
+cities.append(China.HongKong())
+cities.append(China.Shanghai())
+cities.append(China.ShanghaiByDistrict())
 
 for city in cities:
     city.save_csv('data')
